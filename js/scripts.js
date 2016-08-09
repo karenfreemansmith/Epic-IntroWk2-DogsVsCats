49fc3c3f8154b3a2038c74ad472261ed01ef388d
Karen Freeman-Smith and Zachary Matthewstearn <zachary@>
2016-08-09 15:07:57 -0700

    forgot to commit until done...

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
 $(document).ready(function() {
 //alert("jQuery loaded..."); //testing for jQuery setup
-$("button#gocats").click(function() {
-  $("ul#cats").prepend("<li>Meow, meow!</li>");
-  $("ul#dogs").prepend("<li><strong>BARK!! BARK!!!</li>");
-  $(".cats").css("opacity", ".25");
-  $(".dogs").css("opacity", ".99");
-});
-$("button#godogs").click(function() {
-  $("ul#dogs").prepend("<li>Bark, bark!</li>");
-  $("ul#cats").prepend("<li><strong>MEOW!! MEOW!!!</li>");
-  $(".dogs").css("opacity", ".25");
-  $(".cats").css("opacity", ".99");
-});
+  $("button#godogs").click(function() {
+    $("ul#cats").prepend("<li><img src='img/cat_scared.jpg' class='img-responsive img-small'><br><p>Meow, meow!</p></li>");
+
+    $("ul#dogs").prepend("<li><img src='img/dog_fierce.jpg' class='img-responsive img-small'><br><p><strong>BARK!! BARK!!!</strong></p></li>");
+  });
+
+
+  $("button#gocats").click(function() {
+    $("ul#dogs").prepend("<li><img src='img/dog_scared.jpg' class='img-responsive img-small'><br><p>Bark, bark!</p></li>");
+
+    $("ul#cats").prepend("<li><img src='img/cat_fierce.jpg' class='img-responsive img-small'><br><p><strong>MEOW!! MEOW!!!</strong></p></li>");
+  });
+
+
+  $("button").click(function(){
+    $("ul#cats").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#dogs").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
 });
